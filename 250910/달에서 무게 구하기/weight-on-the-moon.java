65a60344754458f75b0b451efa348dc0d7b1821d
YEAJI <166423163+YeaJi5@users.noreply.github.com>
2025-09-10 14:48:11 +0900

    Title: 달에서 무게 구하기, Time: 94ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250910/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int w = 13;
+        float ratio = 0.165f;
+
+        System.out.printf("%d * %f = %.6f", w, ratio, w*ratio);
+    }
+}
