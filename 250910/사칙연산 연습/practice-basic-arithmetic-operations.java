9cf554aa4381bc1ab3d8af81f09519ce48af90cb
YEAJI <166423163+YeaJi5@users.noreply.github.com>
2025-09-10 11:21:09 +0900

    Title: 사칙연산 연습, Time: 94ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250910/사칙연산 연습/practice-basic-arithmetic-operations.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int a = 5;
+        int b = 30;
+        int c = a+b;
+        int r1 = c / 8;
+        int r2 = c % 8;
+
+        System.out.println(r1);
+        System.out.println(r2);
+
+    }
+}
